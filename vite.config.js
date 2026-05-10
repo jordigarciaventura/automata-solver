418b1334091ccfdadd17223fd8087bd4a57c5fec
Jordi García Ventura <jordigarciaventura@gmail.com>
2026-05-10 12:19:12 +0200

    Revert "chore: deploy to cloudflare"
    This reverts commit 0ffa4370b8290c148457fa4880ee584a9f76823d.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-export default defineConfig({
-  base: './',
+export default {
+  base: '/automata-solver/',
   build: {
     outDir: 'dist'
-  },
-  plugins: [
-    cloudflare()
-  ]
-})
+  }
+}
